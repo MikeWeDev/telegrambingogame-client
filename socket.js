379e13a2014bfc9c1fb7218ff88eaa241a62ed72
Mikyas~ <mikyasnegash2@gmail.com>
2026-08-12 18:56:02 +0300

    chnaging socket page to use env values

--- socket.js
+++ socket.js
@@ -1,12 +1,14 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://bingoback.bingoogame.com", {
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+const socket = io(BACKEND_URL, {
   transports: ["websocket"],
-  upgrade:         false,
-  reconnection:    true,
-  reconnectionDelay:      1000,
-  reconnectionAttempts:   5,
-  timeout:         10000,
+  upgrade: false,
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionAttempts: 5,
+  timeout: 10000,
 });
 
 export default socket;
