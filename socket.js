75b9096b4734778ca17a2827fade58c99ca9db8b
Mikyas~ <mikyasnegash2@gmail.com>
2026-08-13 17:13:17 +0300

    trying to fix socket io connection err 2

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://telegrambingogame-server.onrender.com";
+const BACKEND_URL = import.meta.env.BACKEND_URL || "https://telegrambingogame-server.onrender.com";
 
 const socket = io(BACKEND_URL, {
   transports: ["websocket", "polling"],
