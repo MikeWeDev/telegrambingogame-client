4af946e58ccda66e87d0314b30a89ba145e58ba1
Mikyas~ <mikyasnegash2@gmail.com>
2026-08-13 17:03:04 +0300

    trying to fix socket io connection err

--- socket.js
+++ socket.js
@@ -1,13 +1,12 @@
 import { io } from "socket.io-client";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://telegrambingogame-server.onrender.com";
 
 const socket = io(BACKEND_URL, {
-  transports: ["websocket"],
-  upgrade: false,
+  transports: ["websocket", "polling"],
   reconnection: true,
   reconnectionDelay: 1000,
-  reconnectionAttempts: 5,
+  reconnectionAttempts: 10,
   timeout: 10000,
 });
 
